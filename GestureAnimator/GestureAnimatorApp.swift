9650c08909408da66162d6b03cdb4ba844efdbed
iExpertApps <iexpertapps1@gmail.com>
2025-04-18 23:42:42 +0500

    Initial Commit

---
+++ GestureAnimator/GestureAnimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureAnimatorApp.swift
+//  GestureAnimator
+//
+//  Created by Syed Zia ur Rehman on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GestureAnimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
